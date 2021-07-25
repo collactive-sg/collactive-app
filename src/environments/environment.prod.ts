b4446aa1f795e39c6fe57ce3cc439b278bad76b3
Nikhilalalalala <uvnikhi@gmail.com>
2021-07-25 13:24:34 +0800

    Add env details

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCJwBqc09Lw7PmOGtiwhch6AIpWRNSfgCw",
+    authDomain: "collactive-a9ab5.firebaseapp.com",
+    databaseURL: "https://collactive-a9ab5-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "collactive-a9ab5",
+    storageBucket: "collactive-a9ab5.appspot.com",
+    messagingSenderId: "116853725157",
+    appId: "1:116853725157:web:445843f69611a5aef1010c",
+    measurementId: "G-N524KLKDVZ"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD47l7ZBSZCP8rqSWbfxow1niNHjXcgBPc",
+    authDomain: "collactive-stg-4f947.firebaseapp.com",
+    projectId: "collactive-stg-4f947",
+    storageBucket: "collactive-stg-4f947.appspot.com",
+    messagingSenderId: "966252894840",
+    appId: "1:966252894840:web:3b68bf2814d431c95d6b8d",
+    measurementId: "G-TTGCHE61D0"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
