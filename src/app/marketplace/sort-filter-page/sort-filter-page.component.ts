d49900f62a2a262d2d5c1962a1e0d3f387871ef3
Nikhilalalalala <uvnikhi@gmail.com>
2021-09-29 17:38:29 +0800

    Sort UI

--- src/app/marketplace/sort-filter-page/sort-filter-page.component.ts
+++ src/app/marketplace/sort-filter-page/sort-filter-page.component.ts
@@ -8,6 +8,9 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class SortFilterPageComponent implements OnInit {
 
+  isDatePostedSortSelected = false;
+  isDateExpressedSortSelected = false;
+
   constructor(
     public activeModal: NgbActiveModal,
     ) { }
@@ -15,4 +18,34 @@ export class SortFilterPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addDatePostedFilter() {
+    this.isDatePostedSortSelected = !this.isDatePostedSortSelected;
+    if (this.isDatePostedSortSelected) {
+      document.getElementById("date-posted-sort").style.background = "#3B485F"
+      document.getElementById("date-posted-sort").style.color = "#FFFFFF"
+    } else {
+      document.getElementById("date-posted-sort").style.background = "#FFFFFF"
+      document.getElementById("date-posted-sort").style.color = "#3B485F"
+    }
+  }
+
+  addDateExpressedFilter() {
+    this.isDateExpressedSortSelected = !this.isDateExpressedSortSelected;
+    if (this.isDateExpressedSortSelected) {
+      document.getElementById("date-expressed-sort").style.background = "#3B485F"
+      document.getElementById("date-expressed-sort").style.color = "#FFFFFF"
+    } else {
+      document.getElementById("date-expressed-sort").style.background = "#FFFFFF"
+      document.getElementById("date-expressed-sort").style.color = "#3B485F"
+    }
+  }
+
+  applyFilter() {
+    this.activeModal.close();
+  }
+
+
+  close() {
+    this.activeModal.close();
+  }
 }
