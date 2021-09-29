43ae67189c20f7cf615ce18a9cb1c41d9db050f2
Nikhilalalalala <uvnikhi@gmail.com>
2021-09-29 23:27:06 +0800

    Make filter sections

--- src/app/marketplace/sort-filter-page/sort-filter-page.component.ts
+++ src/app/marketplace/sort-filter-page/sort-filter-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -10,12 +11,30 @@ export class SortFilterPageComponent implements OnInit {
 
   isDatePostedSortSelected = false;
   isDateExpressedSortSelected = false;
+  filterForm: FormGroup;
+
+  dietaryRestrictions = [
+    { name: "Halal", checked: false },
+    { name: "Vegan", checked: false },
+    { name: "Vegetarian", checked: false },
+    { name: "Kosher", checked: false },
+    { name: "Pescatarian", checked: false },
+    { name: "Dairy-free", checked: false },
+    { name: "Gluten-free", checked: false },
+    { name: "Nut-free", checked: false },
+    // { name: "Health supplements", checked: false }
+  ]
 
   constructor(
     public activeModal: NgbActiveModal,
     ) { }
 
   ngOnInit(): void {
+    this.filterForm = new FormGroup({
+      milkType: new FormControl('', Validators.required),
+      // healthSupplements: new FormControl("", Validators.required),
+      donorBabyAge: new FormControl("", Validators.required)
+    });
   }
 
   addDatePostedFilter() {
