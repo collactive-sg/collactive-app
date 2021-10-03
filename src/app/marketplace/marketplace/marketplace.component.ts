e8f79e2ed52571bf563528e2f6ccb8e30e1a6ed8
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2021-10-03 16:10:24 +0800

    Add filter posts

--- src/app/marketplace/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace/marketplace.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { element } from 'protractor';
 import { AuthService } from 'src/app/service/auth/auth.service';
+import { FilterService } from 'src/app/service/filter/filter.service';
 import { ListingService } from 'src/app/service/listing/listing.service';
 import { UserDataService } from 'src/app/service/user-data/user-data.service';
 import { SortFilterPageComponent } from '../sort-filter-page/sort-filter-page.component';
@@ -12,7 +14,7 @@ import { SortFilterPageComponent } from '../sort-filter-page/sort-filter-page.co
 })
 export class MarketplaceComponent implements OnInit {
 
-  allLiveListings;
+  liveListings;
   currentUser;
   currentUserData;
   isDonor;
@@ -21,15 +23,16 @@ export class MarketplaceComponent implements OnInit {
     public listingService: ListingService,
     private auth: AuthService,
     private userDataService: UserDataService,
+    private filterService: FilterService,
     private modalService: NgbModal
   ) {}
 
   ngOnInit(): void {
-    this.listingService.getAllLiveListings().pipe().subscribe(res => this.allLiveListings = res);
     this.auth.getUserAuthState()
       .onAuthStateChanged((user) => {
         if (user) {
           this.currentUser = user;
+          this.filterListing(this.currentUser.uid);
           this.userDataService.getUserDoc(user.uid).pipe().subscribe(userData => {
             this.currentUserData = userData;
             this.isDonor = userData['isDonor'];
@@ -40,5 +43,87 @@ export class MarketplaceComponent implements OnInit {
 
   openFilterPage() {
     this.modalService.open(SortFilterPageComponent);
+  }
+
+  filterListing(userId) {
+    let filterPrefs;
+
+    // get saved user filter pref if have
+    this.filterService.getFilter(userId).subscribe(res => {
+      if (res.data() !== undefined) {
+        filterPrefs = res.data();
+        console.log(filterPrefs);
+        return this.listingService.getAllLiveListings().pipe().subscribe(res => {
+          this.liveListings = res;
+
+          // filter by diet
+          let matchedDonorIDArray = [];
+          this.userDataService.getDonorsByDietaryRestrictions(filterPrefs["donorDiet"], filterPrefs["isOnHealthSupplements"])
+          .then(res => {
+            let users = res
+            
+            res.forEach(x => x.data()["userID"] !== undefined? matchedDonorIDArray.push(x.data()["userID"]): null);
+            console.log(matchedDonorIDArray)
+            this.liveListings = this.liveListings.filter(x => matchedDonorIDArray.includes(x["donorID"]));
+            
+            // filter by date expressed
+            if (filterPrefs["isDateExpressedSortSelected"]) {
+              this.liveListings = this.liveListings.sort((a, b) => b["dateExpressed"] - a["dateExpressed"]);
+            }
+
+            // filter by date posted
+            if (filterPrefs["isDatePostedSortSelected"]) {
+              this.liveListings = this.liveListings.sort((a, b) => b["dateCreated"] - a["dateCreated"]);
+            }
+
+            // filter by milk type
+            if (filterPrefs["milkType"]) {
+              this.liveListings = this.liveListings.filter(x => x["typeOfMilk"] === filterPrefs["milkType"]);
+            }
+
+            // filter by baby age
+            if (filterPrefs["donorBabyAge"]) {
+              let option = filterPrefs["donorBabyAge"];
+              let date = new Date();
+              let matches = []
+              if (option == 2) {
+                date.setMonth(date.getMonth() - 6)
+              } else if (option == 3) {
+                date.setMonth(date.getMonth() - 3)
+              }
+              this.userDataService.getAllChildren().then(
+                res => {
+                  console.log(res)
+                  res.forEach(x => {
+                    let day = x.data()["dateOfBirth"]["day"]
+                    let month = x.data()["dateOfBirth"]["month"]
+                    let year = x.data()["dateOfBirth"]["year"]
+                    let dateOfBirth= new Date()
+                    dateOfBirth.setDate(day)
+                    dateOfBirth.setMonth(month)
+                    dateOfBirth.setFullYear(year)
+                    if (option == 1 || option == 2 || option == 3) {
+                      if (dateOfBirth >= date) {
+                        matches.push(x.ref.parent.parent.id)
+                      }
+                    }
+                  })
+                  this.liveListings = this.liveListings.filter(x => matches.includes(x["donorID"]));
+                }
+              );
+              
+              
+            }
+
+          });
+
+        })
+      } else {
+        return this.listingService.getAllLiveListings().pipe().subscribe(res => {
+          this.liveListings = res;
+        });
+      }
+      
+    });
   }
 }
--- src/app/marketplace/sort-filter-page/sort-filter-page.component.ts
+++ src/app/marketplace/sort-filter-page/sort-filter-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/service/auth/auth.service';
+import { FilterService } from 'src/app/service/filter/filter.service';
 
 @Component({
   selector: 'app-sort-filter-page',
@@ -9,6 +11,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class SortFilterPageComponent implements OnInit {
 
+  currentUser;
   isDatePostedSortSelected = false;
   isDateExpressedSortSelected = false;
   filterForm: FormGroup;
@@ -29,6 +32,8 @@ export class SortFilterPageComponent implements OnInit {
 
   constructor(
     public activeModal: NgbActiveModal,
+    private auth: AuthService,
+    private filterService: FilterService
     ) { }
 
   ngOnInit(): void {
@@ -36,6 +41,57 @@ export class SortFilterPageComponent implements OnInit {
       milkType: new FormControl(''),
       // healthSupplements: new FormControl("", Validators.required),
       donorBabyAge: new FormControl('')
+    });
+    this.auth.getUserAuthState().onAuthStateChanged((user) => {
+      if (user) {
+        this.currentUser = user;
+        this.filterService.getFilter(this.currentUser.uid).subscribe(res => {
+          if (res.data() !== undefined) {
+            if (res.data()["isDateExpressedSortSelected"] !== undefined) {
+              this.isDateExpressedSortSelected = res.data()["isDateExpressedSortSelected"];
+              if (this.isDateExpressedSortSelected) {
+                document.getElementById("date-expressed-sort").style.background = "#3B485F"
+                document.getElementById("date-expressed-sort").style.color = "#FFFFFF"
+              } else {
+                document.getElementById("date-expressed-sort").style.background = "#FFFFFF"
+                document.getElementById("date-expressed-sort").style.color = "#3B485F"
+              }
+            }
+
+            if (res.data()["isDatePostedSortSelected"] !== undefined) {
+              this.isDatePostedSortSelected = res.data()["isDatePostedSortSelected"];
+              if (this.isDatePostedSortSelected) {
+                document.getElementById("date-posted-sort").style.background = "#3B485F"
+                document.getElementById("date-posted-sort").style.color = "#FFFFFF"
+              } else {
+                document.getElementById("date-posted-sort").style.background = "#FFFFFF"
+                document.getElementById("date-posted-sort").style.color = "#3B485F"
+              }
+            }
+            
+            if (res.data()['donorDiet'] !== undefined) {
+              this.dietaryRestrictions = res.data()['donorDiet'];
+            }
+
+            if (res.data()['isOnHealthSupplements'] !== undefined) {
+              this.isOnHealthSupplements = res.data()['isOnHealthSupplements'];
+              if (this.isOnHealthSupplements) {
+                this.addHealthSupplementsFilterYes();
+              } else {
+                this.addHealthSupplementsFilterNo();
+              }
+            }
+
+            if (res.data()['milkType'] !== undefined && res.data()['donorBabyAge'] !== undefined) {
+              this.filterForm = new FormGroup({
+                milkType: new FormControl(res.data()['milkType']),
+              // healthSupplements: new FormControl("", Validators.required),
+                donorBabyAge: new FormControl(res.data()['donorBabyAge'])
+              })
+            }
+          }
+        })
+      }
     });
   }
 
@@ -78,12 +134,17 @@ export class SortFilterPageComponent implements OnInit {
   }
 
 
-  get MilkType() { return this.filterForm.get('milkType') }
-  get DonorBabyAge() { return this.filterForm.get('donorBabyAge') }
+  get MilkType() { return this.filterForm.get('milkType').value }
+  get DonorBabyAge() { return this.filterForm.get('donorBabyAge').value }
   
 
   applyFilter() {
     this.activeModal.close();
+    this.filterService.addFilterListings(this.currentUser.uid, this.isDateExpressedSortSelected, 
+      this.isDatePostedSortSelected, this.MilkType, 
+      this.dietaryRestrictions, this.isOnHealthSupplements, this.DonorBabyAge
+    )
+    .then(() => window.location.reload());
   }
 
   close() {
