2abc2729f2f842c69b594dc464268c369d4075f5
Nikhilalalalala <uvnikhi@gmail.com>
2021-10-06 22:31:36 +0800

    Refactor filtering

--- src/app/marketplace/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace/marketplace.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { element } from 'protractor';
 import { AuthService } from 'src/app/service/auth/auth.service';
@@ -18,13 +18,33 @@ export class MarketplaceComponent implements OnInit {
   currentUser;
   currentUserData;
   isDonor;
+  showFilterPage:boolean = false;
+
+  //filter attributes
+  isDatePostedSortSelected:boolean = false;
+  isDateExpressedSortSelected:boolean = false;
+  dietaryRestrictions = [
+    { name: "Halal", checked: false },
+    { name: "Vegan", checked: false },
+    { name: "Vegetarian", checked: false },
+    { name: "Kosher", checked: false },
+    { name: "Pescatarian", checked: false },
+    { name: "Dairy-free", checked: false },
+    { name: "Gluten-free", checked: false },
+    { name: "Nut-free", checked: false },
+    // { name: "Health supplements", checked: false }
+  ]
+
+  milkType:string = "";
+  donorBabyAge:string = "";
+  isOnHealthSupplements:boolean = false;
+
+  @ViewChild(SortFilterPageComponent) sortFilterPage;
 
   constructor(
     public listingService: ListingService,
     private auth: AuthService,
     private userDataService: UserDataService,
-    private filterService: FilterService,
-    private modalService: NgbModal
   ) {}
 
   ngOnInit(): void {
@@ -32,98 +52,122 @@ export class MarketplaceComponent implements OnInit {
       .onAuthStateChanged((user) => {
         if (user) {
           this.currentUser = user;
-          this.filterListing(this.currentUser.uid);
           this.userDataService.getUserDoc(user.uid).pipe().subscribe(userData => {
             this.currentUserData = userData;
             this.isDonor = userData['isDonor'];
           })
         }
       });
+
+    this.listingService.getAllLiveListings().pipe().subscribe(res => {
+        this.liveListings = res;
+      });
+    
   }
 
   openFilterPage() {
-    this.modalService.open(SortFilterPageComponent);
+    console.log(this.isDatePostedSortSelected)
+    this.showFilterPage = true;
   }
 
-  filterListing(userId) {
-    let filterPrefs;
+  closeFilterPage() {
+    this.showFilterPage = false;
+  }
 
+  applyFilter() {
+    this.isDatePostedSortSelected = this.sortFilterPage.isDatePostedSortSelected;
+    this.isDateExpressedSortSelected = this.sortFilterPage.isDateExpressedSortSelected;
+    this.milkType = this.sortFilterPage.milkType;
+    this.donorBabyAge = this.sortFilterPage.donorBabyAge;
+    this.isOnHealthSupplements = this.sortFilterPage.isOnHealthSupplements;
+    this.dietaryRestrictions = this.sortFilterPage.dietaryRestrictions;
+    this.filterListing();
+    this.closeFilterPage()
+  }
+
+  clearFilters() {
+    this.isDatePostedSortSelected = false;
+    this.isDateExpressedSortSelected = false;
+    this.dietaryRestrictions = [
+      { name: "Halal", checked: false },
+      { name: "Vegan", checked: false },
+      { name: "Vegetarian", checked: false },
+      { name: "Kosher", checked: false },
+      { name: "Pescatarian", checked: false },
+      { name: "Dairy-free", checked: false },
+      { name: "Gluten-free", checked: false },
+      { name: "Nut-free", checked: false },
+      // { name: "Health supplements", checked: false }
+    ]
+
+    this.milkType = "";
+    this.donorBabyAge= "";
+    this.isOnHealthSupplements = false;
+  }
+
+  filterListing() {
     // get saved user filter pref if have
-    this.filterService.getFilter(userId).subscribe(res => {
-      if (res.data() !== undefined) {
-        filterPrefs = res.data();
-        console.log(filterPrefs);
-        return this.listingService.getAllLiveListings().pipe().subscribe(res => {
-          this.liveListings = res;
+    this.listingService.getAllLiveListings().pipe().subscribe(res => {
+      this.liveListings = res;
 
-          // filter by diet
-          let matchedDonorIDArray = [];
-          this.userDataService.getDonorsByDietaryRestrictions(filterPrefs["donorDiet"], filterPrefs["isOnHealthSupplements"])
-          .then(res => {
-            let users = res
-            
-            res.forEach(x => x.data()["userID"] !== undefined? matchedDonorIDArray.push(x.data()["userID"]): null);
-            console.log(matchedDonorIDArray)
-            this.liveListings = this.liveListings.filter(x => matchedDonorIDArray.includes(x["donorID"]));
-            
-            // filter by date expressed
-            if (filterPrefs["isDateExpressedSortSelected"]) {
-              this.liveListings = this.liveListings.sort((a, b) => b["dateExpressed"] - a["dateExpressed"]);
-            }
+      // filter by diet
+      let matchedDonorIDArray = [];
+      this.userDataService.getDonorsByDietaryRestrictions(this.dietaryRestrictions, this.isOnHealthSupplements)
+      .then(res => {
+                  
+        res.forEach(x => x.data()["userID"] !== undefined? matchedDonorIDArray.push(x.data()["userID"]): null);
+        console.log(matchedDonorIDArray)
+        this.liveListings = this.liveListings.filter(x => matchedDonorIDArray.includes(x["donorID"]));
+        
+        // filter by date expressed
+        if (this.isDateExpressedSortSelected) {
+          this.liveListings = this.liveListings.sort((a, b) => b["dateExpressed"] - a["dateExpressed"]);
+        }
 
-            // filter by date posted
-            if (filterPrefs["isDatePostedSortSelected"]) {
-              this.liveListings = this.liveListings.sort((a, b) => b["dateCreated"] - a["dateCreated"]);
-            }
+        // filter by date posted
+        if (this.isDatePostedSortSelected) {
+          this.liveListings = this.liveListings.sort((a, b) => b["dateCreated"] - a["dateCreated"]);
+        }
 
-            // filter by milk type
-            if (filterPrefs["milkType"]) {
-              this.liveListings = this.liveListings.filter(x => x["typeOfMilk"] === filterPrefs["milkType"]);
-            }
+        // filter by milk type
+        if (this.milkType) {
+          this.liveListings = this.liveListings.filter(x => x["typeOfMilk"] === this.milkType);
+        }
 
-            // filter by baby age
-            if (filterPrefs["donorBabyAge"]) {
-              let option = filterPrefs["donorBabyAge"];
-              let date = new Date();
-              let matches = []
-              if (option == 2) {
-                date.setMonth(date.getMonth() - 6)
-              } else if (option == 3) {
-                date.setMonth(date.getMonth() - 3)
-              }
-              this.userDataService.getAllChildren().then(
-                res => {
-                  console.log(res)
-                  res.forEach(x => {
-                    let day = x.data()["dateOfBirth"]["day"]
-                    let month = x.data()["dateOfBirth"]["month"]
-                    let year = x.data()["dateOfBirth"]["year"]
-                    let dateOfBirth= new Date()
-                    dateOfBirth.setDate(day)
-                    dateOfBirth.setMonth(month)
-                    dateOfBirth.setFullYear(year)
-                    if (option == 1 || option == 2 || option == 3) {
-                      if (dateOfBirth >= date) {
-                        matches.push(x.ref.parent.parent.id)
-                      }
-                    }
-                  })
-                  this.liveListings = this.liveListings.filter(x => matches.includes(x["donorID"]));
+        // filter by baby age
+        if (this.donorBabyAge) {
+          let option = parseInt(this.donorBabyAge);
+          if (isNaN(option)) return;
+
+          let date = new Date();
+          let matches = []
+          if (option == 2) {
+            date.setMonth(date.getMonth() - 6)
+          } else if (option == 3) {
+            date.setMonth(date.getMonth() - 3)
+          }
+          this.userDataService.getAllChildren().then(
+            res => {
+              console.log(res)
+              res.forEach(x => {
+                let day = x.data()["dateOfBirth"]["day"]
+                let month = x.data()["dateOfBirth"]["month"]
+                let year = x.data()["dateOfBirth"]["year"]
+                let dateOfBirth= new Date()
+                dateOfBirth.setDate(day)
+                dateOfBirth.setMonth(month)
+                dateOfBirth.setFullYear(year)
+                if (option == 1 || option == 2 || option == 3) {
+                  if (dateOfBirth >= date) {
+                    matches.push(x.ref.parent.parent.id)
+                  }
                 }
-              );
-              
-              
+              })
+              this.liveListings = this.liveListings.filter(x => matches.includes(x["donorID"]));
             }
-
-          });
-
-        })
-      } else {
-        return this.listingService.getAllLiveListings().pipe().subscribe(res => {
-          this.liveListings = res;
-        });
-      }
-      
-    });
+          );              
+        }
+      });
+    })
   }
 }
--- src/app/marketplace/sort-filter-page/sort-filter-page.component.ts
+++ src/app/marketplace/sort-filter-page/sort-filter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/app/service/auth/auth.service';
@@ -12,11 +12,10 @@ import { FilterService } from 'src/app/service/filter/filter.service';
 export class SortFilterPageComponent implements OnInit {
 
   currentUser;
-  isDatePostedSortSelected = false;
-  isDateExpressedSortSelected = false;
-  filterForm: FormGroup;
+  @Input() isDatePostedSortSelected;
+  @Input() isDateExpressedSortSelected;
 
-  dietaryRestrictions = [
+  @Input() dietaryRestrictions = [
     { name: "Halal", checked: false },
     { name: "Vegan", checked: false },
     { name: "Vegetarian", checked: false },
@@ -28,69 +27,83 @@ export class SortFilterPageComponent implements OnInit {
     // { name: "Health supplements", checked: false }
   ]
 
-  isOnHealthSupplements = false;
+  @Input() milkType:string;
+  @Input() donorBabyAge:string;
+  @Input() isOnHealthSupplements:boolean;
 
   constructor(
-    public activeModal: NgbActiveModal,
+    // public activeModal: NgbActiveModal,
     private auth: AuthService,
     private filterService: FilterService
     ) { }
 
   ngOnInit(): void {
-    this.filterForm = new FormGroup({
-      milkType: new FormControl(''),
-      // healthSupplements: new FormControl("", Validators.required),
-      donorBabyAge: new FormControl('')
-    });
+
+    this.setColorOfHealthSupplements(this.isOnHealthSupplements)
+    if (this.isDateExpressedSortSelected) {
+      document.getElementById("date-expressed-sort").style.background = "#3B485F"
+      document.getElementById("date-expressed-sort").style.color = "#FFFFFF"
+    } else {
+      document.getElementById("date-expressed-sort").style.background = "#FFFFFF"
+      document.getElementById("date-expressed-sort").style.color = "#3B485F"
+    }
+    console.log(this.isDatePostedSortSelected)
+    if (this.isDatePostedSortSelected) {
+      document.getElementById("date-posted-sort").style.background = "#3B485F"
+      document.getElementById("date-posted-sort").style.color = "#FFFFFF"
+    } else {
+      document.getElementById("date-posted-sort").style.background = "#FFFFFF"
+      document.getElementById("date-posted-sort").style.color = "#3B485F"
+    }
     this.auth.getUserAuthState().onAuthStateChanged((user) => {
       if (user) {
         this.currentUser = user;
-        this.filterService.getFilter(this.currentUser.uid).subscribe(res => {
-          if (res.data() !== undefined) {
-            if (res.data()["isDateExpressedSortSelected"] !== undefined) {
-              this.isDateExpressedSortSelected = res.data()["isDateExpressedSortSelected"];
-              if (this.isDateExpressedSortSelected) {
-                document.getElementById("date-expressed-sort").style.background = "#3B485F"
-                document.getElementById("date-expressed-sort").style.color = "#FFFFFF"
-              } else {
-                document.getElementById("date-expressed-sort").style.background = "#FFFFFF"
-                document.getElementById("date-expressed-sort").style.color = "#3B485F"
-              }
-            }
+        // this.filterService.getFilter(this.currentUser.uid).subscribe(res => {
+        //   if (res.data() !== undefined) {
+        //     if (res.data()["isDateExpressedSortSelected"] !== undefined) {
+        //       // this.isDateExpressedSortSelected = res.data()["isDateExpressedSortSelected"];
+        //       if (this.isDateExpressedSortSelected) {
+        //         document.getElementById("date-expressed-sort").style.background = "#3B485F"
+        //         document.getElementById("date-expressed-sort").style.color = "#FFFFFF"
+        //       } else {
+        //         document.getElementById("date-expressed-sort").style.background = "#FFFFFF"
+        //         document.getElementById("date-expressed-sort").style.color = "#3B485F"
+        //       }
+        //     }
 
-            if (res.data()["isDatePostedSortSelected"] !== undefined) {
-              this.isDatePostedSortSelected = res.data()["isDatePostedSortSelected"];
-              if (this.isDatePostedSortSelected) {
-                document.getElementById("date-posted-sort").style.background = "#3B485F"
-                document.getElementById("date-posted-sort").style.color = "#FFFFFF"
-              } else {
-                document.getElementById("date-posted-sort").style.background = "#FFFFFF"
-                document.getElementById("date-posted-sort").style.color = "#3B485F"
-              }
-            }
+        //     if (res.data()["isDatePostedSortSelected"] !== undefined) {
+        //       this.isDatePostedSortSelected = res.data()["isDatePostedSortSelected"];
+        //       if (this.isDatePostedSortSelected) {
+        //         document.getElementById("date-posted-sort").style.background = "#3B485F"
+        //         document.getElementById("date-posted-sort").style.color = "#FFFFFF"
+        //       } else {
+        //         document.getElementById("date-posted-sort").style.background = "#FFFFFF"
+        //         document.getElementById("date-posted-sort").style.color = "#3B485F"
+        //       }
+        //     }
             
-            if (res.data()['donorDiet'] !== undefined) {
-              this.dietaryRestrictions = res.data()['donorDiet'];
-            }
+        //     if (res.data()['donorDiet'] !== undefined) {
+        //       this.dietaryRestrictions = res.data()['donorDiet'];
+        //     }
 
-            if (res.data()['isOnHealthSupplements'] !== undefined) {
-              this.isOnHealthSupplements = res.data()['isOnHealthSupplements'];
-              if (this.isOnHealthSupplements) {
-                this.addHealthSupplementsFilterYes();
-              } else {
-                this.addHealthSupplementsFilterNo();
-              }
-            }
+        //     if (res.data()['isOnHealthSupplements'] !== undefined) {
+        //       this.isOnHealthSupplements = res.data()['isOnHealthSupplements'];
+        //       if (this.isOnHealthSupplements) {
+        //         this.addHealthSupplementsFilterYes();
+        //       } else {
+        //         this.addHealthSupplementsFilterNo();
+        //       }
+        //     }
 
-            if (res.data()['milkType'] !== undefined && res.data()['donorBabyAge'] !== undefined) {
-              this.filterForm = new FormGroup({
-                milkType: new FormControl(res.data()['milkType']),
-              // healthSupplements: new FormControl("", Validators.required),
-                donorBabyAge: new FormControl(res.data()['donorBabyAge'])
-              })
-            }
-          }
-        })
+        //     if (res.data()['milkType'] !== undefined && res.data()['donorBabyAge'] !== undefined) {
+        //       this.filterForm = new FormGroup({
+        //         milkType: new FormControl(res.data()['milkType']),
+        //       // healthSupplements: new FormControl("", Validators.required),
+        //         donorBabyAge: new FormControl(res.data()['donorBabyAge'])
+        //       })
+        //     }
+        //   }
+        // })
       }
     });
   }
@@ -119,35 +132,36 @@ export class SortFilterPageComponent implements OnInit {
 
   addHealthSupplementsFilterYes() {
     this.isOnHealthSupplements = true;
-    document.getElementById("yes-button").style.background = "#3B485F"
-    document.getElementById("no-button").style.background = "#fff"
-    document.getElementById("yes-button").style.color = "#FFFFFF"
-    document.getElementById("no-button").style.color = "#9896AF"
-    
+    this.setColorOfHealthSupplements(this.isOnHealthSupplements)
   }
+
+  setColorOfHealthSupplements(res:boolean) {
+    document.getElementById("yes-button").style.background = res ? "#3B485F" : "#fff"
+    document.getElementById("no-button").style.background = res ? "#fff" : "#3B485F"
+    document.getElementById("yes-button").style.color = res ? "#FFFFFF" : "#9896AF"
+    document.getElementById("no-button").style.color = res ? "#9896AF" : "#FFFFFF"
+  }
+
   addHealthSupplementsFilterNo() {
     this.isOnHealthSupplements = false;
-    document.getElementById("no-button").style.background = "#3B485F"
-    document.getElementById("yes-button").style.background = "#fff"
-    document.getElementById("no-button").style.color = "#FFFFFF"
-    document.getElementById("yes-button").style.color = "#9896AF"
+    this.setColorOfHealthSupplements(this.isOnHealthSupplements)
   }
 
 
-  get MilkType() { return this.filterForm.get('milkType').value }
-  get DonorBabyAge() { return this.filterForm.get('donorBabyAge').value }
+  // get MilkType() { return this.filterForm.get('milkType').value }
+  // get DonorBabyAge() { return this.filterForm.get('donorBabyAge').value }
   
 
   applyFilter() {
-    this.activeModal.close();
-    this.filterService.addFilterListings(this.currentUser.uid, this.isDateExpressedSortSelected, 
-      this.isDatePostedSortSelected, this.MilkType, 
-      this.dietaryRestrictions, this.isOnHealthSupplements, this.DonorBabyAge
-    )
-    .then(() => window.location.reload());
+    // this.activeModal.close();
+    // this.filterService.addFilterListings(this.currentUser.uid, this.isDateExpressedSortSelected, 
+    //   this.isDatePostedSortSelected, this.MilkType, 
+    //   this.dietaryRestrictions, this.isOnHealthSupplements, this.DonorBabyAge
+    // )
+    // .then(() => window.location.reload());
   }
 
   close() {
-    this.activeModal.close();
+    // this.activeModal.close();
   }
 }
